96410eda20c1411cc4fb8336f9f25ddee2d9f5b0
AmberTang <72559487+TangYanYee@users.noreply.github.com>
2022-10-11 23:49:18 +0800

    swap psc and arr

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -384,9 +384,9 @@ static void MX_TIM8_Init(void)
 
   /* USER CODE END TIM8_Init 1 */
   htim8.Instance = TIM8;
-  htim8.Init.Prescaler = 1000-1;
+  htim8.Init.Prescaler = 168-1;
   htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim8.Init.Period = 168-1;
+  htim8.Init.Period = 1000-1;
   htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim8.Init.RepetitionCounter = 0;
   htim8.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
