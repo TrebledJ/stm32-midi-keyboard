921f9a0acb23869e130e762c2dc7f88d5dc59352
Johnathan <jjjlaw@connect.ust.hk>
2022-12-03 21:20:58 +0800

    reverse dial (max volume should be on the right)
    note that there is some weird jumpiness going on at the far right, so don't twist the dials to the extreme right

--- Core/Inc/volume.hpp
+++ Core/Inc/volume.hpp
@@ -21,7 +21,7 @@ public:
         constexpr uint32_t leeway = 30;
 
         // Clamp it to reduce noise along maximums.
-        uint32_t val = clamp(instance().get_value(), leeway, Dial::MAX - leeway);
+        uint32_t val = clamp(Dial::MAX - instance().get_value(), leeway, Dial::MAX - leeway);
 
         // Avoid noise by reducing bit-depth.
         val >>= 4;
